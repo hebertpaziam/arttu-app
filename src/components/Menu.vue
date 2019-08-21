f66b31b0c8ad0d0fd82d8908ef80dce99e75b444
Hebert Paziam <hebert@neomed.tech>
2019-08-21 00:42:58 -0300

    add font-awesome and applying fallback on avatar

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,14 +1,71 @@
-<template>
+  <template>
   <div class="component-menu">
-    <h1>Menu</h1>
+    <button v-if="isLoggedIn" type="button" class="logged-in">
+      <div class="avatar">
+        <FontAwesomeIcon icon="user-circle" />
+        <span :style="{backgroundImage: `url(${userAvatar})`}" class="picture"></span>
+      </div>
+
+      <div class="greetings">
+        Olá, {{ username }}
+        <FontAwesomeIcon icon="caret-down" />
+      </div>
+    </button>
   </div>
 </template>
 
-<script>
+  <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
-  name: "Menu"
+  name: "Menu",
+  components: {
+    FontAwesomeIcon
+  },
+  data: function() {
+    return {
+      userAvatar: "/images/tattoos/japanese.jpg",
+      isLoggedIn: true,
+      username: "Daniel"
+    };
+  }
 };
 </script>
 
-<style>
+  <style lang="scss">
+.component-menu {
+  > .logged-in {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: none;
+    border: none;
+
+    > .avatar {
+      position: relative;
+      width: 30px;
+      height: 30px;
+      font-size: 30px;
+
+      > .picture {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+      }
+    }
+    > .greetings {
+      margin-left: 10px;
+      font-weight: 700;
+      text-transform: uppercase;
+      @include font-primary-family();
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {faUserCircle, faCaretDown} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserCircle, faCaretDown);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App)
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
